9a0d094270f48dfee3094302d5f6799fbc09fe45
Federico Mansilla <federicomansilla10@gmail.com>
2023-09-07 19:33:01 -0300

    inclusion de seccion comentarios

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 function convertir() {
-    var monto = parseInt(document.getElementById("valor").value);
-    var resultado = 0;
-    var dolar = 593; 
-    var euro = 654;
+    let monto = parseInt(document.getElementById("valor").value);
+    let resultado = 0;
+    let dolar = 593; 
+    let euro = 654;
     if (document.getElementById("primera").checked){
         resultado = monto / dolar;
         alert("El cambio de Pesos a Dolares es $" + resultado.toFixed(2));
@@ -209,3 +209,78 @@ const baseDatos = {
       currency: "USD",
     }).format(n);
   }
+
+  const comments = [];
+
+const inputContainer = document.createElement("div");
+const input = document.createElement("input");
+input.classList.add("input");
+const commentsContainer = document.querySelector("#comments-container");
+
+commentsContainer.appendChild(inputContainer);
+inputContainer.appendChild(input);
+
+input.addEventListener("keydown", (e) => {
+  handleEnter(e, null);
+});
+
+function handleEnter(e, current) {
+  if (e.key === "Enter" && e.target.value != "") {
+    const newComment = {
+      text: e.target.value,
+      likes: 0,
+      responses: [],
+    };
+    if (current === null) {
+      comments.unshift(newComment);
+    } else {
+      current.responses.unshift(newComment);
+    }
+    e.target.value = "";
+    commentsContainer.innerHTML = "";
+    commentsContainer.appendChild(inputContainer);
+    renderComments(comments, commentsContainer);
+  }
+}
+
+function renderComments(arr, parent) {
+  arr.forEach((element) => {
+    const commentContainer = document.createElement("div");
+    commentContainer.classList.add("comment-container");
+    const responsesContainer = document.createElement("div");
+    responsesContainer.classList.add("responses-container");
+    const replyButton = document.createElement("button");
+    const likeButton = document.createElement("button");
+
+    replyButton.textContent = "Respuesta";
+    likeButton.textContent = "Me Gusta";
+    replyButton.addEventListener("click", (e) => {
+      const newInput = inputContainer.cloneNode(true);
+      newInput.value = "";
+      newInput.focus();
+      newInput.addEventListener("keydown", (e) => {
+        handleEnter(e, element);
+      });
+      commentContainer.insertBefore(newInput, responsesContainer);
+    });
+    likeButton.addEventListener("click", (e) => {
+      element.likes++;
+      likeButton.textContent = `${
+        element.likes > 0 ? element.likes : ""
+      } Me gusta`;
+    });
+    const divContent = document.createElement("div");
+    divContent.textContent = element.text;
+    const divActions = document.createElement("div");
+    //commentContainer.appendChild(document.createTextNode(element.text));
+    commentContainer.appendChild(divContent);
+    commentContainer.appendChild(divActions);
+    divActions.appendChild(replyButton);
+    divActions.appendChild(likeButton);
+    commentContainer.appendChild(responsesContainer);
+    if (element.responses.length > 0) {
+      renderComments(element.responses, responsesContainer);
+    }
+    parent.appendChild(commentContainer);
+  });
+}
